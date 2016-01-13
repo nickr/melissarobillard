b05b2400a6b4aa6b87853e96247be63edee88100
Nick Robillard <nick@ewdd.com>
2016-01-12 19:13:58 -0600

    remove debug kit

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,7 +32,7 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
     
-    public $components = array('DebugKit.Toolbar');
+    #public $components = array('DebugKit.Toolbar');
     
     function beforeFilter(){
         parent::beforeFilter();
