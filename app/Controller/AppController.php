2ac97b2479dc79f6374bf40d752f032fe69b5357
Nick Robillard <nick@ewdd.com>
2016-01-12 19:12:43 -0600

    add debug kit

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,6 +31,9 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+    
+    public $components = array('DebugKit.Toolbar');
+    
     function beforeFilter(){
         parent::beforeFilter();
         $this->set('title_for_layout', 'Melissa Robillard Photography');
