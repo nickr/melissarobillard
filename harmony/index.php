270ac9d0553101ee50541365a803a5f94be18078
Nick Robillard <nick@ewdd.com>
2016-01-13 20:27:27 -0600

    add harmony website

---
+++ harmony/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Harmony Family Services, Inc.</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="business.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="container">
+	<div id="topright"><img src="images/logo.jpg" alt="" width="447" height="140"></div>
+
+    <div id="top"></div>
+
+	<div class="cl"></div>
+
+	<div id="gallery"><img src="images/1.jpg" alt="" width="226" height="140"><img src="images/4.jpg" alt="" width="447" height="140"></div>
+	<div id="content">
+		<div id="pad">
+
+      <h3>Harmony Family Services, Inc.</h3>
+
+
+      <p>For over forty years, Harmony Family Services has provided social services in the Abilene area including residential care and assistance for at-risk children and support services for families in crisis.</p>
+        <p>Harmony provides a caring environment for abused or neglected children in our twenty-four hour emergency shelter or in one of our loving foster homes.</p>
+        <p>Runaway or homeless children find safe, temporary shelter at Harmony with counseling services for both the child and family.</p>
+
+        <p>Harmony Family Services.  Providing care and support for children and families in need.</p>
+      	<p>Through the Pauline B. Simpson Trust, Harmony Family Services awards
+			academic scholarships to local university students preparing to work with
+			at-risk youth and families.</p>
+
+
+
+       <p><img src="images/scholarship.jpg" alt="" width="400" height="200" border="1"></p>
+       <p align="center">2007 Scholarship Recipients</p>
+      </div>
+	</div>
+	<div id="news">
+
+    <h1>Contact Information</h1>
+
+
+    <p>305 Grape Street<br />
+      Abilene, TX 79601<br />
+      <a href="mailto:breedhfs@camalott.com ">breedhfs@camalott.com </a><br />
+      Phone: 325.672.7200<br />
+      Fax: 325.672.7201<br />
+
+      <h1>Runaway and Homeless<br />
+
+      Youth Hotline</h1>
+      Toll-free: 1-800-811-9208</p>
+
+		<p>&nbsp;</p>
+
+	<p align="center"><a href="javascript:;" onClick="window.open('http://www.elocallink.tv/vp2/vp3_show.php?cid=7272','elocallinktv','scrollbars=yes,resizable=yes,width=716,height=440,left=0,top=0,ScreenX=0,ScreenY=0');" ;=""><img src="images/videolink.jpg" width="200px" height="125px" border="0" /></a></p>
+
+    <h1>Directions</h1>
+    <p align="center"><a href="http://maps.google.com/maps?f=q&hl=en&geocode=&q=305+Grape+STreet,+Abilene,+TX+79601&sll=41.905508,-84.08428&sspn=0.011722,0.022938&ie=UTF8&z=17&iwloc=addr" target="_blank"><img src="images/test.gif" width="125" height="100" border="0"></a></p>
+
+
+
+		  </div>
+
+<div id="footer">&copy; 2008. Harmony Family Services, Inc.. All Rights Reserved</div>
+</div>
+</body>
+</html>
